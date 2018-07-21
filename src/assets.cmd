5f5c65a218586b2ee8454ca3f8c9653063ee4dd5
Maxim <maxim@smspower.org>
2018-07-21 10:03:30 +0100

    Fixing paths in assets.cmd

--- src/assets.cmd
+++ src/assets.cmd
@@ -1,5 +1,5 @@
-for %%f in (vgms\music\*.vg*) do java -jar "..\PSGLib\PSGlib-master\tools\PSGTool.jar" "%%f"
-for %%f in (vgms\sfx\*_2.vg*) do java -jar "..\PSGLib\PSGlib-master\tools\PSGTool.jar" "%%f" 2
-for %%f in (vgms\sfx\*_23.vg*) do java -jar "..\PSGLib\PSGlib-master\tools\PSGTool.jar" "%%f" 23
-for %%f in (vgms\sfx\*_3.vg*) do java -jar "..\PSGLib\PSGlib-master\tools\PSGTool.jar" "%%f" 3
-for %%f in (images\*.png) do ..\..\Delphi\bmp2tile\bmp2tile.exe "%%f" -tileoffset 0 -savetiles "%%f.tiles.zx7" -savetilemap "%%f.tilemap.zx7" -fullpalette -savepalette "%%f.palette.bin" -exit
+for %%f in (vgms\music\*.vg*) do java -jar "..\..\PSGLib\PSGlib-master\tools\PSGTool.jar" "%%f"
+for %%f in (vgms\sfx\*_2.vg*) do java -jar "..\..\PSGLib\PSGlib-master\tools\PSGTool.jar" "%%f" 2
+for %%f in (vgms\sfx\*_23.vg*) do java -jar "..\..\PSGLib\PSGlib-master\tools\PSGTool.jar" "%%f" 23
+for %%f in (vgms\sfx\*_3.vg*) do java -jar "..\..\PSGLib\PSGlib-master\tools\PSGTool.jar" "%%f" 3
+for %%f in (images\*.png) do "..\..\..\Delphi\bmp2tile\bmp2tile.exe" "%%f" -tileoffset 0 -savetiles "%%f.tiles.zx7" -savetilemap "%%f.tilemap.zx7" -fullpalette -savepalette "%%f.palette.bin" -exit
